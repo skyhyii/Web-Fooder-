c8873e7e076c2d8d4ff278afaa1b19b95d98134e
Zikra <141193760+ZikraAlRizkya@users.noreply.github.com>
2026-06-11 07:08:14 +0700

    ganti api url

--- Fooder/Frontend/script.js
+++ Fooder/Frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://gray-resume-unwitting.ngrok-free.dev";
 
 const pages = document.querySelectorAll(".page");
 const navButtons = document.querySelectorAll(".nav");
